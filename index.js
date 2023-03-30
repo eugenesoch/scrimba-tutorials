117fa231db13259949a2a974b17e585ac485e407
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2023-03-30 05:45:48 +0100

    Updated files to the latest version
    Added total people counter, need to sort the line break issue

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let all = 0
+let allAll = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+    all = count
+
 }
-function save () {
-    console.log(count)
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    allAll = +allAll + +all
+    totalEl.textContent = allAll
+    count = 0
+    countEl.textContent = 0
+    totalEl.textContent + all
+    
 }
-
-
-let username = "per"
-let message = "You have tree new notifications"
-
-console.log(message + ", " + username + "!")
-
-// Create a variable, messageToUser, that contains the message we have logged
-
-let messageToUser = (message + ", " + username + "!")
-console.log(messageToUser)
-
-
-// Create two variables, name and greeting. The name variable should store your name,
-let firstName = "Eugene Soch";
-let greeting  = "Hi, my name is "
-// and the greeting should store e.g. "Hi, my name is "
-let myGreeting = greeting + firstName + "!"
-console.log (myGreeting)
-
-// Create a third variable, myGreeting, that contatenates the two strings
-// Log myGreeting to the console
-
-
-console.log(4 + 5) // 9
-console.log("2" + "4") // "24"
-console.log("5" + 1) // "51"
-console.log(100 + "100") // "100100"
-
-
 
